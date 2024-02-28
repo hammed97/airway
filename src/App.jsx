577b64b1c38fc20e48cad1ef7e216ff165b00c1f
Emmanuel Bobade <emmanuel.bobade@decagon.dev>
2024-02-29 00:00:01 +0100

    This commit contains an update to the boiler_plate

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import {Router, Routes} from "react-router-dom";
+
+function App() {
+
+
+  return (
+    <>
+    <Router>
+        <Routes>
+      
+        </Routes>
+      </Router>
+
+    </>
+  )
+}
+
+export default App
